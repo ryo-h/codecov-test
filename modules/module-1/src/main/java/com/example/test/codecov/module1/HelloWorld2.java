5669fad989f3b3963ea294fba19b826c74d7e64c
ryo-h <ryouhei.0706@gmail.com>
2018-03-09 11:30:19 +0900

    add class for test

---
+++ modules/module-1/src/main/java/com/example/test/codecov/module1/HelloWorld2.java
@@ -0,0 +1,12 @@
+package com.example.test.codecov.module1;
+
+/**
+ * Created by rhazama on 2018/03/09.
+ */
+public class HelloWorld2 {
+
+
+    public String hello() {
+        return new String("hello");
+    }
+}
